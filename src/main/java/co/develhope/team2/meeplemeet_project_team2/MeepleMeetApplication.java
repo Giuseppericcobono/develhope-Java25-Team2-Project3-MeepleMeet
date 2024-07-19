1fe14432bff4d9d89dd6f55b6d37bea6e3dfc57e
Giuseppe <giuseppe.riccobono@hotmail.com>
2024-07-19 16:17:46 +0200

    First commit

---
+++ src/main/java/co/develhope/team2/meeplemeet_project_team2/MeepleMeetApplication.java
@@ -0,0 +1,13 @@
+package co.develhope.team2.meeplemeet_project_team2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeepleMeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeepleMeetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/develhope/team2/meeplemeet_project_team2/MeepleMeetApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.team2.meeplemeet_project_team2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeepleMeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
