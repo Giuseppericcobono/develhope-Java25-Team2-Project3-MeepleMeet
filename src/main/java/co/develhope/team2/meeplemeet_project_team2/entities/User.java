7908c220eb946628fffff7cd038f98739c1add53
ebielle <eryblongo@gmail.com>
2024-07-21 11:49:19 +0200

    user entity without enum

---
+++ src/main/java/co/develhope/team2/meeplemeet_project_team2/entities/User.java
@@ -0,0 +1,74 @@
+package co.develhope.team2.meeplemeet_project_team2.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private Byte age;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    public User() {}
+
+    public User(Integer id, String firstName, String lastName, Byte age, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Byte getAge() {
+        return age;
+    }
+
+    public void setAge(Byte age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
