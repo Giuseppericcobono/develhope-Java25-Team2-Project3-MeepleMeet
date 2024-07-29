800b1b5a1fdb5751153d92e744416f229835bd66
Giuseppe <giuseppe.riccobono@hotmail.com>
2024-07-29 20:32:15 +0200

    Spostata logica su place controller

--- src/main/java/co/develhope/team2/meeplemeet_project_team2/controllers/PlaceController.java
+++ src/main/java/co/develhope/team2/meeplemeet_project_team2/controllers/PlaceController.java
@@ -5,6 +5,8 @@ import co.develhope.team2.meeplemeet_project_team2.entities.Place;
 import co.develhope.team2.meeplemeet_project_team2.services.PlaceService;
 import org.antlr.v4.runtime.misc.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,32 +20,38 @@ public class PlaceController {
     private PlaceService placeService;
 
     @PostMapping("/create")
-    public @ResponseBody Place createPlace(@RequestBody Place place) {
-        return placeService.createAPlace(place);
+    public ResponseEntity<Place> createPlace(@RequestBody Place place) {
+        Place newPlace = placeService.createAPlace(place);
+        return new ResponseEntity<>(newPlace, HttpStatus.CREATED);
     }
 
     @GetMapping("/search/list")
-    public @ResponseBody List<Place> placeList() {
-        return placeService.getListOfPlaces();
+    public @ResponseBody ResponseEntity<List<Place>> placeList() {
+        List<Place> ListPlace = placeService.getListOfPlaces();
+        return new ResponseEntity<>(ListPlace, HttpStatus.OK);
     }
 
     @GetMapping("/search/{id}")
-    public @ResponseBody Place searchPlace(@PathVariable Integer id) {
-        placeService.getPlaceById(id);
+    public @ResponseBody ResponseEntity<Optional<Place>> searchPlace(@PathVariable Integer id) {
+        Optional<Place> place = placeService.getPlaceById(id);
+        return new ResponseEntity<>(place, HttpStatus.OK);
     }
 
     @PutMapping("/update/{id}")
-    public @ResponseBody Place updatePlace(@PathVariable Integer id, @RequestBody @NotNull Place place) {
-        placeService.updatePlace(id, place);
+    public @ResponseBody ResponseEntity<Place> updatePlace(@PathVariable Integer id, @RequestBody @NotNull Place place) {
+        Place updatePlace = placeService.updatePlace(id, place);
+        return new ResponseEntity<>(updatePlace, HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{id}")
-    public void deletePlace(@PathVariable Integer id) {
+    public @ResponseBody ResponseEntity<Place> deletePlace(@PathVariable Integer id) {
         placeService.deletePlaceById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/all")
-    public void deleteAllPlace() {
+    public @ResponseBody ResponseEntity<Place> deleteAllPlace() {
         placeService.deleteAllPlace();
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
