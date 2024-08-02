5326c2f21a0e3e3f74757c396ef88ca171b885da
ebielle <eryblongo@gmail.com>
2024-08-02 13:17:25 +0200

    deleted enrollment

--- src/main/java/co/develhope/team2/meeplemeet_project_team2/repositories/EnrollmentRepository.java
+++
@@ -1,7 +0,0 @@
-package co.develhope.team2.meeplemeet_project_team2.repositories;
-
-import co.develhope.team2.meeplemeet_project_team2.entities.Enrollment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnrollmentRepository extends JpaRepository<Enrollment, Integer> {
-}
