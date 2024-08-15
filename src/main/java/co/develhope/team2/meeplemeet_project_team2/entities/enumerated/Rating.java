6196c6adafb0b51b21dd5ce7f3fd2937e5444f2b
ebielle <eryblongo@gmail.com>
2024-08-15 17:14:44 +0200

    added numeric value to rating

--- src/main/java/co/develhope/team2/meeplemeet_project_team2/entities/enumerated/Rating.java
+++ src/main/java/co/develhope/team2/meeplemeet_project_team2/entities/enumerated/Rating.java
@@ -2,16 +2,22 @@ package co.develhope.team2.meeplemeet_project_team2.entities.enumerated;
 
 public enum Rating {
 
-    STAR1("★☆☆☆☆"),
-    STAR2("★★☆☆☆"),
-    STAR3("★★★☆☆"),
-    STAR4("★★★★☆"),
-    STAR5("★★★★★");
+    STAR1(1.0, "★☆☆☆☆"),
+    STAR2(2.0, "★★☆☆☆"),
+    STAR3(3.0, "★★★☆☆"),
+    STAR4(4.0, "★★★★☆"),
+    STAR5(5.0, "★★★★★");
 
+    private final Double value;
     private final String stars;
 
-    Rating(String stars) {
+    Rating(Double value, String stars) {
+        this.value = value;
         this.stars = stars;
+    }
+
+    public Double getValue() {
+        return value;
     }
 
     public String getStars() {
