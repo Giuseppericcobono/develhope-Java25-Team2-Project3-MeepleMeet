0428f9df5da83dae46e06b459ef42c9fc355c3d2
ebielle <eryblongo@gmail.com>
2024-08-17 13:49:34 +0200

    specify just the id of the user when creating a new review

--- src/main/java/co/develhope/team2/meeplemeet_project_team2/services/ReviewService.java
+++ src/main/java/co/develhope/team2/meeplemeet_project_team2/services/ReviewService.java
@@ -1,7 +1,10 @@
 package co.develhope.team2.meeplemeet_project_team2.services;
 
 import co.develhope.team2.meeplemeet_project_team2.entities.Review;
+import co.develhope.team2.meeplemeet_project_team2.entities.User;
 import co.develhope.team2.meeplemeet_project_team2.repositories.ReviewRepository;
+import co.develhope.team2.meeplemeet_project_team2.repositories.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +16,21 @@ public class ReviewService {
     @Autowired
     private ReviewRepository reviewRepository;
 
-    public Review createReview(Review review){
-        return reviewRepository.save(review);
+    @Autowired
+    private UserRepository userRepository;
+
+    public Review createReview(Review review) {
+        User user = userRepository.findById(review.getUser().getUserId())
+                .orElseThrow(() -> new EntityNotFoundException("User not found"));
+
+        Review review1 = new Review();
+        review1.setUser(user);
+        review1.setEvent(review.getEvent());
+        review1.setDescription(review.getDescription());
+        review1.setRating(review.getRating());
+
+        reviewRepository.save(review1);
+        return review1;
     }
 
     public List<Review> getAllReviews(){
