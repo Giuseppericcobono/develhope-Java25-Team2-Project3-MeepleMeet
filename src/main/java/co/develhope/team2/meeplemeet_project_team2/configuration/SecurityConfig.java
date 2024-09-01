483f05fe348eb7d6db744ab874e53bb19912a675
GiuseppeFavara97 <giuseppe.favara1997@gmail.com>
2024-09-01 18:26:24 +0200

    configurazione della sicurezza e dellgli accessi

---
+++ src/main/java/co/develhope/team2/meeplemeet_project_team2/configuration/SecurityConfig.java
@@ -0,0 +1,63 @@
+package co.develhope.team2.meeplemeet_project_team2.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final DataSource dataSource;
+
+    public SecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/users/register", "/users/login").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/events/create").hasAnyRole("USER", "OWNER")
+                        .requestMatchers("/events/**").hasAnyRole("USER", "OWNER", "ADMIN")
+                        .requestMatchers("/owner/events/**").hasRole("OWNER")
+                        .anyRequest().authenticated()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults()); // Opzionale: Abilita il form di login predefinito di Spring Security
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder auth = http.getSharedObject(AuthenticationManagerBuilder.class);
+
+        // Configurazione dell'autenticazione basata su JDBC con query SQL personalizzate
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("SELECT username, password, true FROM users WHERE username = ?")
+                .authoritiesByUsernameQuery("SELECT username, user_type FROM users WHERE username = ?")
+                .passwordEncoder(passwordEncoder());
+
+        return auth.build();
+    }
+}
